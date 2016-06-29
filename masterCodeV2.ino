8c1d8900ae74958a6001c69bcacf473083685b61
Kunchala Anil <anilkunchalaece@gmail.com>
2016-06-29 13:01:46 +0530

    Modified v2_version
    Modified V2_version 
    Modifications :
    in V2_version the Data from Slave is Coming as String and now it is Coming as Bytes (Int Converted to the Byte) 
    For Reference Check 
    
    http://jamesreubenknowles.com/arduino-i2c-1680
    
    http://forum.arduino.cc/index.php?topic=409677

--- masterCodeV2.ino
+++ masterCodeV2.ino
@@ -5,10 +5,12 @@
 #define slaveC 10
 
 #define byteLength 2
-
+int noOfValues = 0;
 boolean chRead = false;
 char ch;
-
+unsigned long int interval = 10; // Duration for Requesting the Data
+unsigned long int prevMillis = 0;
+unsigned long int newMillis;
 
 void setup() {
   Wire.begin();//Address is Optional For Master
@@ -16,6 +18,7 @@ void setup() {
   Serial.println("Please enter --> a to read the data from Slave 1 ");
   Serial.println("Pleae enter -->  b to read the data from slave 2 ");
   Serial.println("Please enter --> c to read the data from slave 3");
+  Serial.println("Please enter --> i to Change the Duration (Default Duaration is 10 Seconds)");
   
 }
 
@@ -39,11 +42,28 @@ void loop() {
       requestSlaveC();
       break;
 
+      case 'i':
+      changeInterval();
+      break;
+
       default:
       Serial.println("Please check the Command");
       
     }
     chRead = false;
+  }
+
+  newMillis = millis();
+  if(newMillis - prevMillis >= interval*1000){
+    prevMillis = newMillis;
+    requestSlaveA();
+    delay(100);
+    requestSlaveB();
+    delay(100);
+    requestSlaveC();
+    noOfValues++;
+    Serial.print("The No Of vaues Received is -->\t");
+    Serial.println(noOfValues);
   }
 
 }
@@ -82,4 +102,23 @@ int getData(){
     receivedValue |= Wire.read();
     return receivedValue;
  }
+
+
+void changeInterval(){
+  Serial.println("Please enter the New Duration In Seconds");
+  while(!Serial.available())
+  {
+    //Wait until User Responds
+  }
+  interval = Serial.parseInt();
+  if(interval){
+    Serial.print("The New Interval is -->\t");
+    Serial.print(interval);
+    Serial.println("  Seconds");
+    
+  }else{
+    Serial.println("PLease Enter the Valid Number");
+    changeInterval();
+  }
+}
 
